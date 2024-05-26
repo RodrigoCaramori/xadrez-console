9bfec1bcc76eaaa7594ddf7c3ae7703aaf0eee5b
Rodrigo Caramori <rodrigocaramori@gmail.com>
2024-05-27 00:43:26 +0900

    Implementando as restricoes de movimento

--- Program.cs
+++ Program.cs
@@ -15,27 +15,42 @@ namespace xadrez_console
 
                 while (!partida.terminada)
                 {
-                    Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab);
+                    try
+                    {
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab);
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("\n==================");
+                        Console.ResetColor();
+                        Console.WriteLine("Turno: " + partida.turno);
+                        Console.Write("Aguardando jogada: " + partida.jogadorAtual);
 
-                    Console.ForegroundColor = ConsoleColor.DarkCyan;
-                    Console.WriteLine("\n==================");
-                    Console.ResetColor();
-                    Console.Write("\nOrigem: ");
-                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("\n==================");
+                        Console.ResetColor();
+                        Console.Write("\nOrigem: ");
+                        Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
 
-                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
 
-                    Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
 
-                    Console.ForegroundColor = ConsoleColor.DarkCyan;
-                    Console.WriteLine("\n==================");
-                    Console.ResetColor();
-                    Console.Write("\nDestino: ");
-                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("\n==================");
+                        Console.ResetColor();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeDestino(origem, destino);
 
-                    partida.ExecutaMovimento(origem, destino);
+                        partida.realizaJogada(origem, destino);
+                    }
+                    catch (TabuleiroException e)
+                    {
+                        Console.Write(e.Message);
+                        Console.ReadLine();
+                    }
                 }
                 
 
